300cce0ebe638fb1fc23634c4725a695c74d9e87
unknown <robertrey1234@gmail.com>
2019-09-02 19:02:23 -0500

    Created server, imported files, created get, post, and getByID

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// implement your API here
+// Imports
+const express = require('express');
+const server = express();
+const Users = require("./data/db.js");
+server.use(express.json());
+
+// CRUD Endpoints
+server.get('/api/users', (req, res) => {
+    Users.find()
+    .then(users => {
+        res.status(200).json(users)
+    }) 
+    .catch(() => {
+        res.status(500).json( {message: "Could not get user info"})
+    })
+})
+
+// Port Listening
+const port = 8000;
+server.listen(port, () => console.log("Listening on 8000"))
